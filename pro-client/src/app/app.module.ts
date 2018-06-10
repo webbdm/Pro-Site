0da803924d485594043375d4acea5e0314c7d182
webbdm <gwebb762@gmail.com>
2018-06-10 15:03:55 -0500

    Added image and sidenv

--- pro-client/src/app/app.module.ts
+++ pro-client/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideNavComponent } from './navigation/side-nav/side-nav.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
